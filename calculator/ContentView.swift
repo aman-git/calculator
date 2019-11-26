f2be44a22359af88d2b6bd7c8bcf083a261066a6
Amandeep Singh <aman.workmail@gmail.com>
2019-11-27 00:23:55 +0530

    first commit

---
+++ calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by Amandeep Singh on 27/11/19.
+//  Copyright © 2019 Amandeep Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
